da9244730cddefa7e510bdbe9abf84e166175858
David Marx <david.marx84@gmail.com>
2023-04-06 16:08:40 -0700

    abstract a bunch of stuff into a Document class

--- tag.py
+++ tag.py
@@ -1,31 +1,45 @@
-from dataclasses import dataclass
 import openai
 from pathlib import Path
 
 
-@dataclass
 class Document:
-    fpath: Path
-    content: str
+    def __init__(self, fpath: Path):
+        self.fpath = fpath
+        self.parse()
+    def parse(self):
+        lines = self.fpath.read().split('\n')
+        self.title = lines[0]
+        self.tags: set, self.content: str = parse_tags_from_lines(lines[1:])
+    @property
+    def has_tags(self):
+        return len(self.tags) > 0
+    def __str__(self):
+        tags = '  \n'.join(self.tags)
+        return '\n\n'.join([self.title, tags, self.content])
+    def save(self):
+        with open(self.fpath, 'w') as f:
+            f.write(str(self))
 
 
-def parse_tags_from_doc(doc):
+def sort_docs(docs):
     """
-    returns a list of dicts associating documents with their tags
+    separates document collection into two groups: docs with tags, and docs without tags
     """
-    pass
-
+    tags_present = []
+    tags_absent = []
+    for doc in docs:
+        if doc.has_tags:
+            tags_present.append(doc)
+        else:
+            tags_absent.append(doc)
+                           
+                
 def build_prompt(docs_and_tags):
     """
     consumes output from `parse_tags_from_doc()` and formats it for the LLM
     """
     pass
 
-def sort_docs(docs):
-    """
-    separates document collection into two groups: docs with tags, and docs without tags
-    """
-    pass
 
 def predict_tags(doc, prompt):
     """
@@ -33,23 +47,18 @@ def predict_tags(doc, prompt):
     """
     pass
 
-def add_tags(doc, tags):
-    """
-    add new tags to document
-    """
-    pass
 
 def main(docs):
     """
     parse documents, build an LLM tagger, tag docs that don't already have tags.
     """
     tags_present, tags_absent = sort_docs(docs)
-    docs_and_tags = [parse_tags_from_doc(doc) for doc in tags_present]
-    prompt = build_prompt(docs_and_tags)
+    prompt = build_prompt(tags_present)
     for doc in tags_absent:
         tags = predict_tags(doc, prompt)
-        add_tags(doc, tags)
+        doc.tags.update(tags)
+        doc.save()
    
 if __name__ == '__main__':
-    docs = [Document(fpath=path, content=path.read()) for path in Path('.').glob('*.md')]
+    docs = [Document(fpath) for fpath in Path('.').glob('*.md')]
     main(docs)
